5149b69e432c53429feeb84c6f7fb4a047c23420
Bachourian Rafael <bachourian@gmail.com>
2019-05-11 00:52:29 +0200

    Move

--- SQL/GainAbsolu.sql
+++ SQL/GainAbsolu.sql
@@ -1,25 +1,25 @@
 SELECT (
 SELECT SUM(Gagne)-SUM(Mise)
-FROM jouer JOIN pokershand
+FROM jouer JOIN pokershand ON (jouer.PokerSHand_num = pokershand.num) 
 WHERE Joueur_nom="Reathe" 
-) as "Gain_Moyen",
+) as "Gain Total",
 
 (
 SELECT SUM(Gagne)-SUM(Mise)
 FROM jouer
 WHERE Joueur_nom="Reathe" AND Main REGEXP '\\[([1-9TJKQA])[shcd] \\1[shcd]\\]'
-) as "Gain_Moyen_pocket_pair",
+) as "Gain Total pocket pair",
 
 (
 SELECT SUM(Gagne)-SUM(Mise)
 FROM jouer
 WHERE Joueur_nom="Reathe" AND Main REGEXP '\\[[1-9TJKQA]([shcd]) [1-9TJKQA]\\1\\]'
-) as "Gain_Moyen_suited_hand",
+) as "Gain Total suited hand",
 
 (
 SELECT SUM(Gagne)-SUM(Mise)
 FROM jouer
 WHERE Joueur_nom="Reathe" AND Main REGEXP '\\[([AKQJT])[shcd] \\1[shcd]\\]'
-) as "Gain_Moyen_pocket_pair_T_plus"
+) as "Gain Total pocket pair T plus"
 FROM DUAL;
 
--- SQL/GainMoyen.sql
+++ SQL/GainMoyen.sql
@@ -9,17 +9,17 @@ as "Gain Moyen",
 SELECT AVG(Gagne)-AVG(Mise)
 FROM jouer LEFT JOIN pokershand ON (jouer.PokerSHand_num = pokershand.num)
 WHERE Joueur_nom="Reathe" AND Main REGEXP '\\[([1-9TJKQA])[shcd] \\1[shcd]\\]' AND LaDate >= '2019-04-08 10:00:00'
-) as "Gain_Moyen_pocket_pair",
+) as "Gain Moyen pocket pair",
 
 (
 SELECT AVG(Gagne)-AVG(Mise)
 FROM jouer LEFT JOIN pokershand ON (jouer.PokerSHand_num = pokershand.num)
 WHERE Joueur_nom="Reathe" AND Main REGEXP '\\[[1-9TJKQA]([shcd]) [1-9TJKQA]\\1\\]' AND LaDate >= '2019-04-08 10:00:00'
-) as "Gain_Moyen_suited_hand",
+) as "Gain Moyen suited hand",
 
 (
 SELECT AVG(Gagne)-AVG(Mise)
 FROM jouer LEFT JOIN pokershand ON (jouer.PokerSHand_num = pokershand.num)
 WHERE Joueur_nom="Reathe" AND Main REGEXP '\\[([AKQJT])[shcd] \\1[shcd]\\]' AND LaDate >= '2019-04-08 10:00:00'
-) as "Gain_Moyen_pocket_pair_T_plus"
+) as "Gain Moyen pocket pair T plus"
 FROM DUAL
